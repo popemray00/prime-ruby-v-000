cdd703b133b02021c2c4e321aa28a8ca2464576c
Michael Pope <popemray1986@gmail.com>
2017-10-09 00:05:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,10 @@
-def prime?(num)
 
+  def prime?(integer)
+  return false if integer <= 1
+  return true if integer <= 3
+    if (2...integer-1).any?{ |i| integer % i == 0}
+      false
+    else
+      true
+    end
 end
